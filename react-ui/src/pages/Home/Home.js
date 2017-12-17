88b93952a63a5bdecdfb48adaecf4d353e390904
Darren Kwong <kwongdarren@gmail.com>
2017-12-17 00:35:32 -0800

    array sort function nearly complete

--- react-ui/src/pages/Home/Home.js
+++ react-ui/src/pages/Home/Home.js
@@ -13,11 +13,64 @@ class Home extends Component {
     	API.AllReviews()
     	.then(res => {
 			// console.log(res)
-			let newArray = res;
-			console.log(newArray);
+			let dataArray = res;
+			console.log(dataArray);
+			let reviews = res.data[0].reviews;
 			let date = res.data[0].reviews[0].query_date;
 			console.log(date);
-			console.log(moment(date).format('LL'));
+			console.log(reviews);
+			// console.log(moment(date).format('LL'));
+			let firstdate = date.replace(/ .*/,''); //extract date from timestamp
+			console.log('firstdate: ' + firstdate);
+			console.log('res.data.length: ' + res.data.length)
+
+			const dateArray = res.data;
+			console.log(dateArray);
+
+			let comparisonDate = dateArray[0].reviews[0].query_date //date of first array item to compare against
+			// const sortedArray = [dateArray[0].reviews[0].review_count]; //create array with initial value
+			console.log(sortedArray);
+
+			const sortedArray = [];
+			var inner = [];
+
+			for (var i = 0; i < dateArray.length; i++) {
+				let querydate = dateArray[i].reviews[0].query_date;
+				console.log('querydate: ' + querydate);
+				
+
+				console.log('comparison date: ' + comparisonDate);
+				if (comparisonDate === querydate){
+					inner.push(dateArray[i].reviews[0].review_count); //push review count into array
+				} else {
+					sortedArray.push(inner);
+					inner = [dateArray[i].reviews[0].review_count];
+					console.log('inner: ' + inner);
+				}
+				querydate = dateArray[i].reviews[0].query_date;
+				console.log('querydate: ' + querydate);
+			}
+
+			console.log(sortedArray);
+			// arrsplit = dateArray.reduce(function (acc, val) {
+		 //        var inner;
+
+		 //        if (acc.previous !== val) {
+		 //            inner = [];
+		 //        } else {
+		 //            inner = acc.newArray.pop();
+		 //        }
+
+		 //        inner.push(val);
+		 //        acc.previous = val;
+		 //        acc.newArray.push(inner);
+
+		 //        return acc;
+		 //    }, {
+		 //        previous: null,
+		 //        newArray: []
+		 //    });
+
     	})
     	.catch(err => console.log(err));
     };
