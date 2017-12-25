4d1cb7e18525f9ce9eff28fe2607e89636f9e93f
Darren Kwong <kwongdarren@gmail.com>
2017-12-25 00:53:51 -0800

    basic restaurant info displayed in dom

--- react-ui/src/pages/findRestaurant/findRestaurant.js
+++ react-ui/src/pages/findRestaurant/findRestaurant.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Input, Form, Searchbtn } from "../../components/Form";
+import { Searched, Searcheditems } from "../../components/Searched";
 import API from "../../utils/API.js";
 import "./findRestaurant.css";
 
@@ -9,8 +10,21 @@ class findRestaurant extends Component {
 
 	state = {
 		restaurantArr: [],
-		restaurantName: "Homeroom"
+		restaurantName: "Homeroom",
+		restaurantInfo: {}
 	};
+
+	componentDidMount() {
+    	API.testQuery(this.state.restaurantName)
+			.then(res => {
+				this.setState({
+					restaurantInfo: res.data
+				})
+				console.log(res);
+				console.log(this.state);
+			})
+			.catch(err => console.log(err));
+  	}
 
 	loadRestaurants = () => {
     	API.AllReviews()
@@ -41,7 +55,11 @@ class findRestaurant extends Component {
 		if (this.state.restaurantName) {
 			API.testQuery(this.state.restaurantName)
 			.then(res => {
-				console.log(res)
+				this.setState({
+					restaurantInfo: res.data
+				})
+				console.log(res);
+				console.log(this.state);
 			})
 			.catch(err => console.log(err));
 		}
@@ -51,7 +69,7 @@ class findRestaurant extends Component {
 		return (
 		<div>
 			<h1>
-				Home Home Home
+				Find A Restaurant
 			</h1>
 			<form>
               <Input
@@ -67,6 +85,25 @@ class findRestaurant extends Component {
                Search Restaurant
               </Searchbtn>
             </form>
+
+            {this.state.restaurantInfo.length ? (
+              <Searched>
+                {this.state.restaurantInfo.map(restaurant => (
+                  <Searcheditems key={restaurant._id}>              
+					<p> Name of Restaurant: {restaurant.name} </p>
+					<p> Address: {restaurant.location.address}, {restaurant.location.city}, {restaurant.location.state} </p>
+					<p> Data Summary: 
+						<ul>
+							<li>Yelp Rating: {restaurant.rating[0].rating} </li>
+							<li>Yelp URL: {restaurant.yelpURL} </li>
+						</ul>
+					</p>
+                  </Searcheditems>
+                ))}
+              </Searched>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
 
 			<button onClick={this.loadRestaurants}>
 				load restaurants
