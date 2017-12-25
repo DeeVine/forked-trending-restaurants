544cea69b4ebf8a1bf1634bc1d5d7e888703f4bf
Darren Kwong <kwongdarren@gmail.com>
2017-12-24 21:05:03 -0800

    added in search form to restaurant page

--- react-ui/src/pages/Restaurant/Restaurant.js
+++ react-ui/src/pages/Restaurant/Restaurant.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Input, Form, Searchbtn } from "../../components/Form";
 import API from "../../utils/API.js";
 import "./Restaurant.css";
 
@@ -15,41 +16,6 @@ class Restaurant extends Component {
     	API.AllReviews()
     	.then(res => {
 			console.log(res)
-			// let dataArray = res;
-			// console.log(dataArray);
-			// let reviews = res.data[0].reviews;
-			// let date = res.data[0].reviews[0].query_date;
-			// console.log(date);
-			// console.log(reviews);
-			// // console.log(moment(date).format('LL'));
-			// let firstdate = date.replace(/ .*/,''); //extract date from timestamp
-			// console.log('firstdate: ' + firstdate);
-			// console.log('res.data.length: ' + res.data.length)
-			// const dateArray = res.data;
-			// console.log(dateArray);
-
-			// let comparisonDate = dateArray[0].reviews[0].query_date //date of first array item to compare against
-			// // // const sortedArray = [dateArray[0].reviews[0].review_count]; //create array with initial value
-			// // console.log(sortedArray);
-
-			// const sortedArray = [];
-			// var inner = [];
-
-			// for (var i = 0; i < dateArray.length; i++) {
-			// 	let querydate = dateArray[i].reviews[0].query_date;
-			// 	if (comparisonDate === querydate){
-			// 		inner.push(dateArray[i].reviews[0].review_count); //push review count into array
-			// 	} else {
-			// 		sortedArray.push(inner);
-			// 		inner = [dateArray[i].reviews[0].review_count];
-			// 	}
-			// 	comparisonDate = querydate;
-			// 	//if last array item, push inner array into sortedArray;
-			// 	if(i === dateArray.length-1){
-			// 		sortedArray.push(inner);
-			// 	}
-			// }
-			// console.log(sortedArray);
     	})
     	.catch(err => console.log(err));
     };
@@ -62,17 +28,48 @@ class Restaurant extends Component {
     	.catch(err => console.log(err));
     };
 
+    handleInputChange = event => {
+		const { name, value } = event.target;
+		this.setState({
+		  [name]: value
+		});
+		console.log(this.state.restaurantName);
+	};
+
+	handleFormSubmit = event => {
+		event.preventDefault();
+		if (this.state.restaurantName) {
+			API.testQuery(this.state.restaurantName)
+			.then(res => {
+				console.log(res)
+			})
+			.catch(err => console.log(err));
+		}
+	};
+
 	render() {
 		return (
 		<div>
 			<h1>
 				Home Home Home
 			</h1>
+			<form>
+              <Input
+                value={this.state.restaurantName}
+                onChange={this.handleInputChange}
+                name="restaurantName"
+                placeholder="restaurant"
+              />
+              <Searchbtn
+                disabled={!(this.state.restaurantName)}
+                onClick={this.handleFormSubmit}
+              >
+               Search Restaurant
+              </Searchbtn>
+            </form>
+
 			<button onClick={this.loadRestaurants}>
 				load restaurants
-			</button>
-			<button onClick={() => this.testQuery(this.state.restaurantName)}>
-				load test query
 			</button>
 
 			<button onClick={() => this.getAPIData()}>
