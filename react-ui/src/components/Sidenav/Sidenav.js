3cb6d5f4b7f38b931e9c2d0d7165e8256baa25e0
Darren Kwong <kwongdarren@gmail.com>
2017-12-30 19:34:53 -0800

    fgsfgsf

---
+++ react-ui/src/components/Sidenav/Sidenav.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import "./Sidenav.css";
+
+
+// Depending on the current path, this component sets the "active" classNameNameName on the appropriate navigation link item
+const Sidenav = () =>
+  <div className='sidenav'>
+    sidenav
+  </div>
+
+export default Sidenav;
