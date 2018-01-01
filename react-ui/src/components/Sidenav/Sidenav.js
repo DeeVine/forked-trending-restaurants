6b4789ee0db5f90ab5692c46759a8d5e3547536f
Darren Kwong <kwongdarren@gmail.com>
2018-01-01 02:45:21 -0800

    ui is responsive

--- react-ui/src/components/Sidenav/Sidenav.js
+++ react-ui/src/components/Sidenav/Sidenav.js
@@ -2,7 +2,6 @@ import React from "react";
 // import { Link } from "react-router-dom";
 import "./Sidenav.css";
 
-// Depending on the current path, this component sets the "active" classNameNameName on the appropriate navigation link item
 const Sidenav = () =>
   <div className='sidenav'>
   	<div className='profile'>
