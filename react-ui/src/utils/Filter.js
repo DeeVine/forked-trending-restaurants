e7937e958edf4f17f0dd5db7104a73728c272869
Jonathan Tung <jtung23@gmail.com>
2018-01-06 18:54:52 -0800

    geolocate doesnt work, testing if works on diff computer

--- react-ui/src/utils/Filter.js
+++ react-ui/src/utils/Filter.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import Mathy from './Mathy.js'
 export default {
 	getTop10ByScore: function(sortArr) {
 		const top10 = []
@@ -19,12 +19,44 @@ export default {
 	// logic: plug in array of restaurants
 	// filter by query_date. get an array with each unique day
 	// get average for each day.
+	convertDate: function(utc) {
+		let queryDate = utc.replace(/ .*/,'');
+		return queryDate
+	},
 
-	dailyDiffAvg: function(resData) {
-		let labels = resData.checkins.map(checkins => {
-			let queryDate = checkins.query_date.replace(/ .*/,'');
-			console.log(queryDate)
+	dailyDiffAvg: function(dataArr) {
+		let obj = {}
+		dataArr.forEach(item => {
+			let uniqueDateArr = []
+			let diffandDatesArr = Mathy.getDiffwithDate(item.checkins, 'checkins')
+			console.log(diffandDatesArr)
+			diffandDatesArr.map(each => {
+				each.query_date = this.convertDate(each.query_date)
+				let queryDate = each.query_date
+				let index = uniqueDateArr.findIndex(x => x === queryDate)
+
+				if (index === -1) {
+					uniqueDateArr.push(queryDate)
+				}
+			})
+
+			// loop through uniqueDateArr dates and use date to
+			// filter by date to get average for each day
+			// return day average as object for past 14 days
+			// filters dates into indiv arrays dynamically
+			
+			uniqueDateArr.forEach(value => {
+				let filteredDateArr = item.checkins.filter(each => each.query_date === value)
+				
+				if (obj.hasOwnProperty(value)) {
+					obj[value].push(filteredDateArr[0].checkins)
+				} else {
+					obj[value] = [filteredDateArr[0].checkins]
+				}
+				
+			})		
 		})
+
 	}
 
 }
